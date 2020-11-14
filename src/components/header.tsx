979126e82187695dafc4f2a81208fdad0dd0252c
Fumiya Goto <fumiya.goto@bizreach.co.jp>
2020-11-14 11:42:49 +0900

    fix structure

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,7 +6,6 @@ import styles from '../styles/header.module.css'
 
 const navItems: { label: string; page?: string; link?: string }[] = [
   { label: 'Home', page: '/' },
-  { label: 'Blog', page: '/blog' },
 ]
 
 const ogImageUrl = 'https://notion-blog.now.sh/og-image.png'
--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -20,12 +20,12 @@ export async function getStaticProps({ params: { slug }, preview }) {
   const post = postsTable[slug]
 
   // if we can't find the post or if it is unpublished and
-  // viewed without preview mode then we just redirect to /blog
+  // viewed without preview mode then we just redirect to /posts
   if (!post || (post.Published !== 'Yes' && !preview)) {
     console.log(`Failed to find post for slug: ${slug}`)
     return {
       props: {
-        redirect: '/blog',
+        redirect: '/posts',
         preview: false,
       },
       unstable_revalidate: 5,
